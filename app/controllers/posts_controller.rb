33fcf03ae8285dec955d1e8797c0dda3721fa8ef
Cody <codybarker.or@gmail.com>
2023-06-03 15:22:18 -0700

    s

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  #rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def show
     post = Post.find(params[:id])
@@ -10,9 +10,6 @@ class PostsController < ApplicationController
     post = Post.find(params[:id])
     post.update!(post_params)
     render json: post
-  rescue ActiveRecord::RecordInvalid => invalid
-    render json: render json: {errors: invalid.record.errors}, status: :unprocessable_entity
-
   end
 
   private
@@ -21,8 +18,37 @@ class PostsController < ApplicationController
     params.permit(:category, :content, :title)
   end
 
+  def render_not_found_response
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end
+
   # def render_unprocessable_entity_response
   #   render json: {errors: invalid.record.errors}, status: :unprocessable_entity
   # end
 
+  # class PostsController < ApplicationController
+
+#   def show
+#     post = Post.find(params[:id])
+#     render json: post
+#   end
+
+#   def update
+#     post = Post.find(params[:id])
+#     post.update!(post_params)
+#     render json: post
+#   rescue ActiveRecord::RecordInvalid => invalid
+#     render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+#   end
+
+#   private
+
+#   def post_params
+#     params.permit(:category, :content, :title)
+#   end
+
+# end
+
 end
+
+
